4b11ab9d9c8741e9ad0cb39b879bd218f14b9bd1
Andre Staltz <andre@staltz.com>
2023-11-28 10:08:05 +0200

    fix tests

--- test/util.js
+++ test/util.js
@@ -7,14 +7,14 @@ const Keypair = require('ppppp-keypair')
 function createPeer(opts) {
   if (opts.name) {
     const tmp = os.tmpdir()
-    opts.path ??= path.join(tmp, `ppppp-sync-${opts.name}-${Date.now()}`)
+    opts.db ??= {path: path.join(tmp, `ppppp-sync-${opts.name}-${Date.now()}`)}
     opts.keypair ??= Keypair.generate('ed25519', opts.name)
     opts.name = undefined
   }
-  if (!opts.path) throw new Error('need opts.path in createPeer()')
+  if (!opts.db.path) throw new Error('need opts.db.path in createPeer()')
   if (!opts.keypair) throw new Error('need opts.keypair in createPeer()')
 
-  rimraf.sync(opts.path)
+  rimraf.sync(opts.db.path)
   return require('secret-stack/bare')()
     .use(require('secret-stack/plugins/net'))
     .use(require('secret-handshake-ext/secret-stack'))
